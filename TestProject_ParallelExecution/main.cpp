8f549d50e1529dace2612479e8aaf804ea3fc27a
Jan S. (Milania1) <github@milania.de>
2016-10-05 11:51:23 +0200

    Small refactoring

--- TestProject_ParallelExecution/main.cpp
+++ TestProject_ParallelExecution/main.cpp
@@ -2,61 +2,61 @@
 #include <ctime>
 #include <atomic>
 
-std::deque<double> serialExecution(const std::deque<double>& inputValues)
+std::deque<double> serialExecution(const std::deque<double>& input)
 {
-	std::deque<double> outputValues;
-	for (size_t i = 0; i < inputValues.size(); i++)
+	std::deque<double> resultVariable;
+	for (size_t i = 0; i < input.size(); i++)
 	{
 		double value = 0.0;
-		for (size_t j = 0; j < inputValues.size(); j++)
+		for (size_t j = 0; j < input.size(); j++)
 		{
-			value += std::log10(std::sqrt(std::pow(inputValues[i], 3)) * std::sin(j) + std::cos(j) * std::cos(i));
+			value += std::log10(std::sqrt(std::pow(input[i], 3)) * std::sin(j) + std::cos(j) * std::cos(i));
 		}
 
 		if (static_cast<int>(value) % 2 == 0)
 		{
-			outputValues.push_back(value);
+			resultVariable.push_back(value);
 		}
 
 		if (i % 1000 == 0)
 		{
-			std::cout << "Progess: " << static_cast<double>(i) / static_cast<double>(inputValues.size() - 1) << std::endl;
+			std::cout << "Progess: " << static_cast<double>(i) / static_cast<double>(input.size() - 1) << std::endl;
 		}
 	}
 
-	return outputValues;
+	return resultVariable;
 }
 
-std::deque<double> parallelExecution(const std::deque<double>& inputValues)
+std::deque<double> parallelExecution(const std::deque<double>& input)
 {
-	/* Note: the code is just an example for the usuage of the ParallelExecution class and does not really cover an useful scenario */
+	/* Note: the code is just an example for the usage of the ParallelExecution class and does not really cover an useful scenario */
 	
 	ParallelExecution pe;
 
-	std::deque<double> outputValues;
-	pe.parallel_for(0ull, inputValues.size() - 1, [&](const size_t i)	// Outer for loop uses as much threads as cores are available on the system
+	std::deque<double> resultVariable;
+	pe.parallel_for(0, input.size() - 1, [&](const size_t i)		// Outer for loop uses as much threads as cores are available on the system
 	{
 		std::atomic<double> value = 0.0;
-		pe.parallel_for(0, inputValues.size() - 1, [&](const size_t j)	// Inner for loop uses 2 threads explicitly (the parallelisation at this point does not really make sense, just for demonstration purposes)
+		pe.parallel_for(0, input.size() - 1, [&](const size_t j)	// Inner for loop uses 2 threads explicitly (the parallelisation at this point does not really make sense, just for demonstration purposes)
 		{
-			value = value + std::log10(std::sqrt(std::pow(inputValues[i], 3)) * std::sin(j) + std::cos(j) * std::cos(i));	// Do some (weird) computation
+			value = value + std::log10(std::sqrt(std::pow(input[i], 3)) * std::sin(j) + std::cos(j) * std::cos(i));					// Do some (weird) computation
 		}, 2);
 
-		if (static_cast<int>(value) % 2 == 0)	// Store the value only if some condition holds (direct index access would not require thread-safe storage of the result value)
+		if (static_cast<int>(value) % 2 == 0)		// Store the value only if some condition holds (direct index access would not require thread-safe storage of the result value)
 		{
 			pe.setResult([&] ()
 			{
-				outputValues.push_back(value);	// Store the result value in a thread-safe way
+				resultVariable.push_back(value);	// Store the result value in a thread-safe way
 			});
 		}
 
-		if (i % 1000 == 0)	// Just to output not too much
+		if (i % 1000 == 0)							// Just to output not too much
 		{
-			pe.write(std::string("Progess: ") + std::to_string(static_cast<double>(i) / static_cast<double>(inputValues.size() - 1)));	// Give a threads safe console output
+			pe.write(std::string("Progess: ") + std::to_string(static_cast<double>(i) / static_cast<double>(input.size() - 1)));	// Gives a threads-safe console output
 		}
 	});
 	
-	return outputValues;
+	return resultVariable;
 }
 
 int main()
